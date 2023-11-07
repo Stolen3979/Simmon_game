3615ab3d01665871f03ee74df3770b9d9b10982f
Stolen3979 <126186981+Stolen3979@users.noreply.github.com>
2023-11-06 22:36:43 -0500

    Add files via upload

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,79 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+// var start = false;
+// $("#start").on("click", function() {
+//    start = true;
+// } );
+// $("#end").on("click", function() {
+//     start = false;
+//  } );
+
+// while(start === true){
+
+//     
+// for (var j=0;j<= gamePattern.length;j++){
+//     if (gamePattern[j]!=userClickedPattern[j]){
+//         start= false;
+//     } else{
+//         start = true;
+//     }
+// }
+// }
+ function nextSquence()
+{
+
+    var randomNumber = Math.floor(Math.random()*3 +1);
+    var randomChosenColour = buttonColours[randomNumber];
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+   
+    return gamePattern.push(randomChosenColour);
+}
+
+
+// nextSquence();
+// gamePattern.push(nextSquence());
+
+
+function playSound(name){
+    switch(name){
+        case "green":
+            green= new Audio("./sounds/green.mp3");
+            green.play();
+        break;
+        case "red":
+            red = new Audio("./sounds/red.mp3");
+            red.play();
+        break;
+        case "yellow":
+            yellow = new Audio("./sounds/yellow.mp3");
+            yellow.play();
+        break;
+        case "blue":
+            blue = new Audio("./sounds/blue.mp3");
+            blue.play();
+        break;
+        default:
+            wrong = new Audio("./sounds/wrong.mp3");
+            wrong.play();
+        break;
+
+    }
+}
+
+if(gamePattern.length>0){
+    for (var i = 0;i<= gamePattern.length-1;i++){
+    playSound(gamePattern[i]);
+    }
+}
+$(".btn").on("click",function() {
+    userClickedPattern.push(this.id);
+    $("#"+this.id+"").toggleClass(".pressed");
+} );
+
+function animatePress(currentColour){
+   
+    
+}
+
+
